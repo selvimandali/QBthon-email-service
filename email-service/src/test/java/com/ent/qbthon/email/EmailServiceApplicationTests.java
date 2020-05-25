0d646e4ea30fd166bcffdc3a30cae23f19a8af2f
ctsjavauser176 <ctsjavauser176@JavaCTS12.iiht.tech>
2020-05-25 18:51:41 +0530

    initial commit

---
+++ email-service/src/test/java/com/ent/qbthon/email/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ent.qbthon.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
